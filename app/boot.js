68039f214855ea1e5bf834abe68653dab3261fea
Aaron Chavarria <aaron.chavarria@rivetlogic.com>
2016-06-03 16:35:10 -0600

    production mode

--- app/boot.js
+++ app/boot.js
@@ -1,7 +1,7 @@
-System.register(['@angular/platform-browser-dynamic', "./app.component", "@angular/http"], function(exports_1, context_1) {
+System.register(['@angular/platform-browser-dynamic', "./app.component", "@angular/http", '@angular/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var platform_browser_dynamic_1, app_component_1, http_1;
+    var platform_browser_dynamic_1, app_component_1, http_1, core_1;
     return {
         setters:[
             function (platform_browser_dynamic_1_1) {
@@ -12,11 +12,15 @@ System.register(['@angular/platform-browser-dynamic', "./app.component", "@angul
             },
             function (http_1_1) {
                 http_1 = http_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
             }],
         execute: function() {
+            core_1.enableProdMode();
             platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS]);
         }
     }
 });
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImJvb3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7OztZQUtBLG9DQUFTLENBQUMsNEJBQVksRUFBQyxDQUFDLHFCQUFjLENBQUMsQ0FBQyxDQUFDIiwiZmlsZSI6ImJvb3QuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLy88cmVmZXJlbmNlIHBhdGg9XCIuLi90eXBpbmdzL2Jyb3dzZXIuZC50c1wiLz5cbmltcG9ydCB7Ym9vdHN0cmFwfSBmcm9tICdAYW5ndWxhci9wbGF0Zm9ybS1icm93c2VyLWR5bmFtaWMnO1xuaW1wb3J0IHtBcHBDb21wb25lbnR9IGZyb20gXCIuL2FwcC5jb21wb25lbnRcIjtcbmltcG9ydCB7SFRUUF9QUk9WSURFUlN9IGZyb20gXCJAYW5ndWxhci9odHRwXCI7XG5cbmJvb3RzdHJhcChBcHBDb21wb25lbnQsW0hUVFBfUFJPVklERVJTXSk7XG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImJvb3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7OztZQU1BLHFCQUFjLEVBQUUsQ0FBQztZQUVqQixvQ0FBUyxDQUFDLDRCQUFZLEVBQUMsQ0FBQyxxQkFBYyxDQUFDLENBQUMsQ0FBQyIsImZpbGUiOiJib290LmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8vPHJlZmVyZW5jZSBwYXRoPVwiLi4vdHlwaW5ncy9icm93c2VyLmQudHNcIi8+XG5pbXBvcnQge2Jvb3RzdHJhcH0gZnJvbSAnQGFuZ3VsYXIvcGxhdGZvcm0tYnJvd3Nlci1keW5hbWljJztcbmltcG9ydCB7QXBwQ29tcG9uZW50fSBmcm9tIFwiLi9hcHAuY29tcG9uZW50XCI7XG5pbXBvcnQge0hUVFBfUFJPVklERVJTfSBmcm9tIFwiQGFuZ3VsYXIvaHR0cFwiO1xuaW1wb3J0IHtlbmFibGVQcm9kTW9kZX0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XG5cbmVuYWJsZVByb2RNb2RlKCk7XG5cbmJvb3RzdHJhcChBcHBDb21wb25lbnQsW0hUVFBfUFJPVklERVJTXSk7XG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
--- dev/boot.ts
+++ dev/boot.ts
@@ -2,5 +2,8 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from "./app.component";
 import {HTTP_PROVIDERS} from "@angular/http";
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
 
 bootstrap(AppComponent,[HTTP_PROVIDERS]);
